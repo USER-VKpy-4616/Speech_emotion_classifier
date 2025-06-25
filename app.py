2ca62497077e36085e91d8f166748182ff60cb3e
VKpy-4616 <vaisantiit@gmail.com>
2025-06-25 23:50:24 +0530

    drive link

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import gdown
 MODEL_PATH = "emotion_cnn_mel.keras"
 if not os.path.exists(MODEL_PATH):
     with st.spinner("Downloading model..."):
-        gdown.download("https://drive.google.com/file/d/1onb8BIr7D6-b6Pnyp6rDn_JXcJOmBe5W/view?usp=drive_link", MODEL_PATH, quiet=False)
+        gdown.download("https://drive.google.com/uc?id=1onb8BIr7D6-b6Pnyp6rDn_JXcJOmBe5W", MODEL_PATH, quiet=False)
 
 model = tf.keras.models.load_model(MODEL_PATH)
 le = joblib.load("label_encoder.pkl")
